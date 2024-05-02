7aa02060180ab29f7168fe6b503814f0f8a7ff1d
elinlim22 <elinlim22@gmail.com>
2024-05-02 19:16:39 +0900

    packet ids test

--- SocketServer/PacketDefine.cs
+++ SocketServer/PacketDefine.cs
@@ -12,29 +12,35 @@ public enum PacketType : UInt16
     InSessionConnected = 0,
     InSessionDisconnected,
     /* ----------------------------------- 로그인 ---------------------------------- */
-    LoginRequest = 1000,
+    LoginRequest = 1002,
     LoginResponse,
     /* ---------------------------------- 방 입장 ---------------------------------- */
-    EnterRoomRequest = 2000,
+    EnterRoomRequest = 1015,
     EnterRoomResponse,
     NotifyRoomUserList,
     NotifyRoomNewUser,
     /* ---------------------------------- 방 나가기 --------------------------------- */
-    NotifyRoomUserLeft = 3000,
-    LeaveRoomRequest,
+    NotifyRoomUserLeft = 1023,
+    LeaveRoomRequest = 1021,
     LeaveRoomResponse,
-    NotifyUserMustClose,
+    NotifyUserMustClose = 1005,
     /* ---------------------------------- 방 채팅 ---------------------------------- */
-    ChatRequest = 4000,
+    ChatRequest = 1026,
     ChatResponse,
     NotifyRoomChat,
     /* ---------------------------------- 게임 시작 --------------------------------- */
-    StartGameRequest = 5000,
-    StartGameResponse,
+    // StartGameRequest = 5000,
+    // StartGameResponse,
+    PKTReqReadyOmok = 1031,
+    PKTResReadyOmok,
+    PKTNtfReadyOmok,
+    PKTNtfStartOmok,
     /* ---------------------------------- 돌 두기 ---------------------------------- */
-    PutStoneRequest = 6000,
+    PutStoneRequest = 1035,
     PutStoneResponse,
+    NotifyPutStone,
     /* ---------------------------------- 게임 종료 --------------------------------- */
-    EndGameRequest = 7000,
-    EndGameResponse,
+    // EndGameRequest = 7000,
+    // EndGameResponse,
+    PKTNtfEndOmok = 1038,
 }
